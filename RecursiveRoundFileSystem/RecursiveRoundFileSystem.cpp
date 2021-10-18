51a7ba7cad73caa91f37d01388aae7e30036ed94
MihailKolyasov <fanarik.penza@yandex.ru>
2021-10-19 01:04:37 +0300

    Добавьте файлы проекта.

---
+++ RecursiveRoundFileSystem/RecursiveRoundFileSystem.cpp
@@ -0,0 +1,11 @@
+﻿// RecursiveRoundFileSystem.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    // create project
+    std::cout << "Hello World!\n";
+}
+
